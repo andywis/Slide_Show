648793688f0902e3dea40b35f5d69d7195509358
Andy Watkins <awatkins@ukcloud.com>
2018-09-14 13:54:50 +0100

    Added a digital clock (http://flipclockjs.com)

--- default_settings.py
+++ default_settings.py
@@ -17,3 +17,13 @@ delay_between_slides = 5
 
 # Name of the template file (you can usually leave this alone)
 template = "slideshow_1.html"
+
+# Whether to display the digital clock
+# For the digital clock, ensure flipclock.min.js, flipclock.css
+# and jquery-3.2.1.min.js are installed in the images/ folder.
+show_digital_clock = True
+
+# Further settings for the digital clock
+show_clock_seconds = False
+clock_pos_left = '60%'
+clock_pos_top = '95%'
--- make_slideshow.py
+++ make_slideshow.py
@@ -4,33 +4,45 @@ import os
 
 from jinja2 import Environment, FileSystemLoader
 
+
 # To customise the settings, copy the file "default_settings.py"
 # and save it as "settings.py". See comments in default_settings.py
 # for explanations.
 if os.path.exists("settings.py"):
     import settings
 else:
-     import default_settings as settings
+    import default_settings as settings
 
-# Find all the images
-image_filenames = [os.path.join(settings.images_folder, fn) 
-    for fn in os.listdir(settings.images_folder) 
-    if os.path.isfile(os.path.join(settings.images_folder, fn))]
 
-# Build the context to pass on to the Jinja template
-context = {
-    'width': settings.screen_size['x'],
-    'height': settings.screen_size['y'],
-    'delay': settings.delay_between_slides,
-    'image_filenames': sorted(image_filenames),
-    }
+def find_all_images(folder):
+    imgs = []
+    for fn in os.listdir(settings.images_folder):
+        if os.path.isfile(os.path.join(settings.images_folder, fn)):
+            if fn.split('.')[-1] in ['JPG', 'jpg']:
+                imgs.append(os.path.join(settings.images_folder, fn))
+    return imgs
 
-# Load and render the template.
-template_env = Environment(loader=FileSystemLoader('templates'))
-template = template_env.get_template(settings.template)
-result = template.render(context)
+if __name__ == '__main__':
+    image_filenames = find_all_images(settings.images_folder)
 
-# Save
-with open(settings.output_filename, "w") as fh:
-    print "Saving file to {}".format(settings.output_filename)
-    fh.write(result)
+    # Build the context to pass on to the Jinja template
+    context = {
+        'width': settings.screen_size['x'],
+        'height': settings.screen_size['y'],
+        'delay': settings.delay_between_slides,
+        'image_filenames': sorted(image_filenames),
+        'show_digital_clock': settings.show_digital_clock,
+        'show_clock_seconds': str(settings.show_clock_seconds).lower(),
+        'clock_pos_left': settings.clock_pos_left,
+        'clock_pos_top': settings.clock_pos_top,
+        }
+
+    # Load and render the template.
+    template_env = Environment(loader=FileSystemLoader('templates'))
+    template = template_env.get_template(settings.template)
+    result = template.render(context)
+
+    # Save
+    with open(settings.output_filename, "w") as fh:
+        print "Saving file to {}".format(settings.output_filename)
+        fh.write(result)
